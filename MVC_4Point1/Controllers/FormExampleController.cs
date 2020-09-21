15dabb972f0623d3bd5bab6b83d2353cbf3a2bd3
harpreet <Harpreet Kour>
2020-09-21 14:17:16 -0600

    In class practice solution;Create page;In class practice 2;

--- MVC_4Point1/Controllers/FormExampleController.cs
+++ MVC_4Point1/Controllers/FormExampleController.cs
@@ -8,20 +8,32 @@ namespace MVC_4Point1.Controllers
 {
     public class FormExampleController : Controller
     {
+        // In Class Practice
+        // #1: Add a Link to the Nav that points to the Input page.
+        // #2: Research and implement a redirect from Index() to Input().
+        // #3: Modify the output on the Output page to add some fun facts about the data inputted.
+        // - First Letter of First Name
+        // - First Letter of Last Name (Assume 2 word names)
+        // - Length of First Name
+        // - Length of Last Name
+        // - Domain of Email (ex "example.com")
+        // - Area code of phone number (Assume 1-###-234-3456 OR ###-456-9876)
+
+        // #4: Null-coalesce the parameters of Output(). 
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Input");
         }
         public IActionResult Input()
         {
             return View();
         }
-
+       
         public IActionResult Output(string name, string email, string phone)
         {
-            ViewBag.Name = name;
-            ViewBag.Email = email;
-            ViewBag.Phone = phone;
+            ViewBag.Name = name ?? "No Name Provided";
+            ViewBag.Email = email ?? "No Email Provided";
+            ViewBag.Phone = phone ?? "No Phone Provided";
 
             return View();
         }
